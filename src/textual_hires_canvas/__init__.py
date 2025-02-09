932986f03f8b63f52474e6e7caa5c6897add2897
David Fokkema <davidfokkema@icloud.com>
2025-02-09 15:14:51 +0100

    Add two examples

--- src/textual_hires_canvas/__init__.py
+++ src/textual_hires_canvas/__init__.py
@@ -1,4 +1,4 @@
-from textual_hires_canvas.canvas import Canvas
+from textual_hires_canvas.canvas import Canvas, TextAlign
 from textual_hires_canvas.hires import HiResMode
 
-__all__ = ["Canvas", "HiResMode"]
+__all__ = ["Canvas", "HiResMode", "TextAlign"]
